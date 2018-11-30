a448257cd0aafd3d793e9366762955d64d3ed61a
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-11-30 15:23:04 +0800

    parcel创建项目

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Button from "./button"
+Vue.component('v-button',Button)
+new Vue({
+    el:'#app'
+})
---
+++ src/button.vue
@@ -0,0 +1,48 @@
+<template>
+    <section >
+        <button class='v-button'>按钮</button>
+    </section>
+</template>
+
+<script >
+export default {
+    name:'',
+    components: {},
+
+    props: {},
+
+    data() {
+        return {
+
+        }
+
+    },
+
+    watch: {},
+
+    methods: {},
+
+    mounted() {},
+
+    }
+</script>
+
+<style scoped lang='scss'>
+    .v-button{
+        font-size:14px ;
+        height:60px;
+        padding:0 1em;
+        border-radius:4px;
+        border: 1px solid #ccc;
+        background: #fff;
+        &:hover{
+            border-color:red;
+        }
+        &:active{
+            background: #fff;
+        }
+        &:focus{
+            outline:none;
+        }
+    }
+</style>
