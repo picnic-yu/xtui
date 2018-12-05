6149a2d5e944d46b6f11fc526f7541ed09fe6111
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-12-05 14:00:53 +0800

    修改button测试文件

--- test/button.test.js
+++ test/button.test.js
@@ -1,76 +1,105 @@
 import chai from 'chai';
 import spies from 'chai-spies';
-
 import Vue from 'vue';
 // import sinon from 'sinon'
 // import sinonChai from 'sinon-chai'
 import Button from '../src/component/button/index.vue'
 const expect = chai.expect;
+chai.use(spies);
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
 describe('Button.vue', () => {
   it('存在.', () => {
     expect(Button).to.exist
   })
-  // it('可以设置icon.', () => {
-  //   const wrapper = mount(Button, {
-  //     propsData: {
-  //       icon: 'loading'
-  //     }
-  //   })
-  //   const useElement = wrapper.find('use')
-  //   expect(useElement.attributes()['href']).to.equal('#icon-loading')
-  // })
-  // it('可以设置loading.', () => {
-  //   const wrapper = mount(Button, {
-  //     propsData: {
-  //       icon: 'loading',
-  //       loading: true
-  //     }
-  //   })
-  //   const vm = wrapper.vm
-  //   const useElements = vm.$el.querySelectorAll('use')
-  //   expect(useElements.length).to.equal(1)
-  //   expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading')
-  // })
+  it('可以设置icon.', () => {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button)
+    // 动态创建组件
+    const button = new Constructor({
+        propsData:{
+            icon:'qiandao' 
+        }
+    });
+    button.$mount(div);
+    let useElement = button.$el.querySelector('use');
+    expect(useElement.getAttribute('xlink:href')).to.eq('#icon-qiandao');
+    button.$el.remove();
+    button.$destroy();
+  })
+  it('可以设置loading.', () => {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button)
+    // 动态创建组件
+    const button = new Constructor({
+        propsData:{
+            // icon:'qiandao',
+            loading:true
+        }
+    });
+    button.$mount(div);
+    let useElement = button.$el.querySelector('use');
+    expect(useElement.getAttribute('xlink:href')).to.eq('#icon-loading');
+    button.$el.remove();
+    button.$destroy();
+  })
 
-  // it('icon 默认的 order 是 1', () => {
-  //   const wrapper = mount(Button, {
-  //     attachToDocument: true,
-  //     propsData: {
-  //       icon: 'loading',
-  //     }
-  //   })
-  //   const vm = wrapper.vm
-  //   const icon = vm.$el.querySelector('svg')
-  //   expect(getComputedStyle(icon).order).to.eq('1')
-  // })
+  it('icon 默认的 order 是 1', () => {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button)
+    // 动态创建组件
+    const button = new Constructor({
+        propsData:{
+            icon:'qiandao',
+            // loading:true
+        }
+    });
+    button.$mount(div);//元素不挂载就不会渲染css  css就没有
+    let svg = button.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);//获取style
+    expect(order).to.eq('1');
+    button.$el.remove();
+    button.$destroy();
+  })
 
-  // it('设置 position 可以改变 order', () => {
-  //   const wrapper = mount(Button, {
-  //     attachToDocument: true,
-  //     propsData: {
-  //       icon: 'loading',
-  //       position: 'right'
-  //     }
-  //   })
-  //   const vm = wrapper.vm
-  //   const icon = vm.$el.querySelector('svg')
-  //   expect(getComputedStyle(icon).order).to.eq('2')
-  // })
-  // it('点击 button 触发 click 事件', () => {
-  //   const wrapper = mount(Button, {
-  //     propsData: {
-  //       icon: 'loading',
-  //     }
-  //   })
-  //   const vm = wrapper.vm
+  it('设置 position 可以改变 order', () => {
+		const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button)
+    // 动态创建组件
+    const button = new Constructor({
+        propsData:{
+            icon:'qiandao',
+            position:'right'
+        }
+    });
+    button.$mount(div);//元素不挂载就不会渲染css  css就没有
+    let svg = button.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);//获取style
+    expect(order).to.eq('2');
+    button.$el.remove();
+    button.$destroy();
+  })
+  it('点击 button 触发 click 事件', () => {
+    const Constructor = Vue.extend(Button)
+    // 动态创建组件
+    const vm = new Constructor({
+        propsData:{
+            icon:'qiandao'
+        }
+    });
+    vm.$mount();
+    // 函数的mock
+    let spy = chai.spy(function(){
 
+    })
+    vm.$on('click',spy)
+    const button = vm.$el.querySelector('button');
+    button.click();
+    expect(spy).to.have.been.called();//期待spy被调用了
 
-  //   const callback = sinon.fake();
-  //   vm.$on('click', callback)
-  //   vm.$el.click()
-  //   expect(callback).to.have.been.called
-
-  // })
+  })
 })
