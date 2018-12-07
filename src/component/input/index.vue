84e34a512872e8bf388266266c0354610f2e0fe7
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-12-07 14:01:23 +0800

    input添加头部icon

--- src/component/input/index.vue
+++ src/component/input/index.vue
@@ -1,9 +1,12 @@
 <template>
-    <section>
+    <section class='v-input-wrap'>
         <template v-if="type !== 'textarea'">
+            <span :class='{["v-input-icon-prefix"]:prefix,[`v-input-icon-prefix-${size}`]:true}' v-if='prefix'>
+                <v-icon class="icon" :name='prefix'></v-icon>
+            </span>
             <input
                 ref="input"
-                :class="{[`v-input-${size}`]:true,[`v-input`]:true}"
+                :class="{[`v-input-${size}`]:true,[`v-input`]:true,['v-input-prefix']:prefix}"
                 :type="type"
                 :placeholder="placeholder">
         </template>
@@ -16,9 +19,12 @@
 </template>
 
 <script>
+import vIcon from '../icon/index';
 export default {
     name:'v-button',
-    components: {},
+    components: {
+        vIcon
+    },
 
     props: {
         type:{
@@ -32,7 +38,13 @@ export default {
         size:{
             type:String,
             default:''
+        },
+        // 头部icon
+        prefix:{
+            type:String,
+            default:''
         }
+        // 尾部
     },
 
     data() {
@@ -52,37 +64,68 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.v-input{
-    display: inline-block;
-    width: 100%;
-    height: 32px;
-    line-height: 1.5;
-    padding: 4px 7px;
-    font-size: 12px;
-    border: 1px solid #dcdee2;
-    border-radius: 4px;
-    color: #515a6e;
-    background-color: #fff;
-    background-image: none;
+.v-input-wrap{
     position: relative;
-    cursor: text;
-    &:hover{
-        border-color: #57a3f3;
+    .v-input{
+        display: inline-block;
+        width: 100%;
+        height: 32px;
+        line-height: 1.5;
+        padding: 4px 7px;
+        font-size: 12px;
+        border: 1px solid #dcdee2;
+        border-radius: 4px;
+        color: #515a6e;
+        background-color: #fff;
+        background-image: none;
+        position: relative;
+        cursor: text;
+        &:hover{
+            border-color: #57a3f3;
+        }
+        &:focus{
+            outline: 0;
+            box-shadow: 0 0 0 2px rgba(45,140,240,.2);
+            border-color: #57a3f3;
+        }
     }
-    &:focus{
-        outline: 0;
-        box-shadow: 0 0 0 2px rgba(45,140,240,.2);
-        border-color: #57a3f3;
+    .v-input-large{
+        font-size: 14px;
+        padding: 6px 7px;
+        height: 36px;
+    }
+    .v-input-small{
+        padding: 1px 7px;
+        height: 24px;
+        border-radius: 3px;
+    }
+    .v-input-prefix{
+        padding-left:32px;
+    }
+    // 头部icon 默认样式
+    .v-input-icon-prefix{
+        width:32px;
+        height:100%;
+        position: absolute;
+        top:0;
+        left:0;
+        z-index: 1;
+        display: inline-block;
+        vertical-align: middle; 
+        padding: 2px 7px;
+        .icon{
+            width:1em;
+            height:1em;
+        }
+    }
+    // 头部icon large样式
+    .v-input-icon-prefix-large{
+        padding: 4px 6px;
+    }
+    // 头部icon small样式
+    .v-input-icon-prefix-small{
+        padding: 0px 6px;
     }
 }
-.v-input-large{
-    font-size: 14px;
-    padding: 6px 7px;
-    height: 36px;
-}
-.v-input-small{
-    padding: 1px 7px;
-    height: 24px;
-    border-radius: 3px;
-}
+
 </style>
