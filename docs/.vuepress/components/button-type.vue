181c66a615f3e94eba7f0e10346327ba26f4e4bb
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-12-27 09:53:48 +0800

    修改button文档

---
+++ docs/.vuepress/components/button-type.vue
@@ -0,0 +1,53 @@
+<template>
+    <section >
+        <v-button  type='error'>按钮</v-button>
+        <v-button  >按钮</v-button>
+        <v-button  type='info'>按钮</v-button>
+        <v-button  type='success'>按钮</v-button>
+        <v-button  type='primary'>按钮</v-button>
+        <v-button  type='waring'>按钮</v-button>
+        
+        <pre>
+            <code>{{content}}</code>
+        </pre>
+        
+    </section>
+</template>
+
+<script>
+import vButton from '../../../src/packages/components/button/button';
+import '../../../src/assets/js/svg.js'
+export default {
+    components: {
+        "v-button":vButton
+    },
+
+    props: {},
+
+    data() {
+        return {
+            content:`
+            <v-button  type='error'>按钮</v-button>
+            <v-button  >按钮</v-button>
+            <v-button  type='info'>按钮</v-button>
+            <v-button  type='success'>按钮</v-button>
+            <v-button  type='primary'>按钮</v-button>
+            <v-button  type='waring'>按钮</v-button>`
+        }
+
+    },
+
+    watch: {},
+
+    methods: {},
+
+    mounted() {
+        
+    },
+
+}
+</script>
+
+<style  scoped>
+ 
+</style>
--- docs/button/README.md
+++ docs/button/README.md
@@ -1,7 +1,23 @@
 # 按钮
 
-使用方法
+按钮类型
 
 <ClientOnly>
-  <button-demos/>
+  <button-type/>
 </ClientOnly>
+
+按钮icon
+
+<ClientOnly>
+  <button-icon/>
+</ClientOnly>
+
+
+
+props
+| 属性           | 说明                                                              | 类型  |默认值|
+| -------------  |:-------------:                                                   | -----:|    -----: |
+| type           | 按钮类型，可选值为 primary、info、success、warning、error或者不设置 | String |       ''   |
+| icon      | 设置按钮的图标类型                                                         |   String |      ''    |
+| position  | 设置按钮图标位置 可选值为 left、right、                       |    String |      'left'     |
+| loading  | 设置loading、                       |    Boolean |      false     |
